7e01c1110cfcff1f70b56fd9a1b6ab8a6c8f0793
mdudhuman <mdudhuman@gmail.com>
2025-02-19 21:56:04 -0500

    Adding java files

---
+++ FirstJavaProgram.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class FirstJavaProgram 
+{
+	public static void main(String args[]) 
+	{
+		System.out.println("Welcome to Java");
+		System.out.println("Sum of value: "+(10+30));
+		
+		/* System.out.println("Sum of value: "+(10+30));
+		System.out.println("Sum of value: "+(10+30));
+		System.out.println("Sum of value: "+(10+30));
+		System.out.println("Sum of value: "+(10+30));
+		System.out.println("Sum of value: "+(10+30));
+		*/
+	}
+}
